7c47d0a5ecc95bb87b50e42af283562715ebd8f3
Stella Tigre <stella.cannefax@gmail.com>
2016-09-06 04:18:02 -0500

    office space default video

--- js/config.js
+++ js/config.js
@@ -65,7 +65,7 @@ _videodrone = {
         videoDefaults: [
             "ggLTPyRXUKc",
             "ZC5U9Pwd0kg",
-            "A9grEa_zSIc"
+            "N9wsjroVlu8"           // office space printer scene
         ]
     }
 }
